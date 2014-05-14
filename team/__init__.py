469a55ab56edcd8a73ef37fd9257a56af1fb29b0
Greg Zynda <gjzynda@indiana.edu>
2014-05-14 12:11:07 -0400

    split annotation to helper file

--- team/__init__.py
+++ team/__init__.py
@@ -18,7 +18,7 @@ class methExperiment:
 		self.samples = {}
 		self.gff = {}
 		self.fa = "Unset"
-		self.probs = {}
+		self.emissions = {}
 		self.windowSize = 500.0
 	def addSample(self, name, samples = []):
 		self.samples[name] = []
@@ -86,37 +86,46 @@ class methExperiment:
 	def makeEmissions(self): # generate emission probabilies from makeMethBins
 		print "Emission Probabilities"
 		for sampleName in self.samples:
-			self.probs[sampleName] = {}
+			self.emissions[sampleName] = {}
 			for gff in self.gff:
-				self.probs[sampleName][gff] = {}
+				self.emissions[sampleName][gff] = {}
 				print gff
 				for methType in methTypes:
 					Data = self.dataDict[sampleName][gff][methType]
 					hist, edges = np.histogram(Data, bins=10, range=(0,1)) # edges are half open [0, 1)
-					self.probs[sampleName][gff][methType] = hist
+					self.emissions[sampleName][gff][methType] = hist
 					string = "%s:\t" % (methType)
 					for i in hist:
 						string += "%d\t" % (i)
 					print string + "sum: %d" % (sum(hist))
 			print edges
-	def writeEmissions(self):
-		for sampleName in self.samples:
-			cPickle.dump(self.probs, open(sampleName+"_probs.pickle",'wb'))
+		cPickle.dump(self.emissions, open("emissions.pickle",'wb'))
 	def readEmissions(self):
-		# loads self.probs[sampleName][gff][methType] array of 10 hist values.
+		if not checkFiles(['emissions.pickle']):
+			# loads self.emissions[sampleName][gff][methType] array of 10 hist values.
+			self.emissions = cPickle.load(open("emissions.pickle",'rb'))
+		else:
+			print "Make emissions first"
+	def printEmissions(self):
 		for sampleName in self.samples:
-			cPickle.load(open(sampleName+"_probs.pickle",'rb'))
-	def makeTransisions(self): # makes transition probabilities
+			for gff in self.gff:
+				print gff
+				for methType in methTypes:
+					string = "%s:\t" % (methType)
+					hist = self.emissions[sampleName][gff][methType]
+					for i in hist:
+						string += "%d\t" % (i)
+					print string + "sum: %d" % (sum(hist))
+	def makeTransitions(self): # makes transition probabilities
 		gffStats = {}
 		for gffFile in self.gff:
 			if self.gff[gffFile] != "NC":
 				gffStats[gffFile] = simpleStats(gffFile)
-		self.transMatrix = simpleAnalyze(gffStats, self.gff, self.fa+'.fai', self.windowSize)
+		self.transMatrix = makeTransMatrix(gffStats, self.gff, self.fa+'.fai', self.windowSize)
 
 def getChromSizes(inFai): # returns a dicationary of chromosome sizes
 	chromLens = {}
-	for line in open(inFai,'r'):
-		#Chr1	30427671	6	79	80
+	for line in open(inFai,'r'):	#Chr1	30427671	6	79	80
 		tmp = line.split('\t')
 		chromLens[tmp[0]] = int(tmp[1])
 	return chromLens
@@ -136,9 +145,9 @@ def simpleStats(inFile): # returns the count and average size of a feature type
 	print "\tAverage Size: %.1f" % (total/float(count))
 	return (count, total/float(count))
 
-def simpleAnalyze(gffStats, gff, inFai, windowSize): #generate transition probabilities
-	chromDict = getChromSizes(inFai)
-	genomeBases = sum(chromDict.values())
+def makeTransMatrix(gffStats, gff, inFai, windowSize): #generate transition probabilities
+	chromSizes = getChromSizes(inFai)
+	genomeBases = sum(chromSizes.values())
 	#self.gff[gffFile] = name
 	#NC	TG	G	PG	TE
 	letterIndex = gff.values()
@@ -220,7 +229,7 @@ def gff_gen(inFile, minSize):
 		tmp = line.split('\t')
 		chrom = tmp[0]
 		strand = tmp[6]
-		start, end = map(int, tmp[3:4])
+		start, end = map(int, tmp[3:5])
 		idNum = tmp[8].split(';')[0]
 		idNum = idNum.split('=')[1]
 		if end-start > minSize:
@@ -234,186 +243,3 @@ def makeBoxplot(sp, dataList, methType, names, fig):
 	if sp != "313":
 		plt.setp(ax.get_xticklabels(), visible=False)
 		plt.setp(ax.get_xticklines(), visible=False)
-
-if __name__ == "__main__":
-	main()
-
-def regionFinder(E,TP,fa,chromLens,states): #get this integrated
-	#TP[from][to]
-	# Emissions ##############
-	#[ 0.   0.1  0.2  0.3  0.4  0.5  0.6  0.7  0.8  0.9  1. ]
-	#[0 1)
-	#("NC","G","TG","TE","PG")
-
-	#get emission probabilities parsed like this
-	#pass in state name order
-	#E[state][meth][count][index]
-
-	EP = parseEP(E) #EP[state][meth][bin]
-
-	for sampleName in self.samples:
-		methIndexes = []
-		for mf in self.samples[sampleName]:
-			methIndexes.append(MethIndex(mf,fa))
-			useVoting(chromLens, experiment, fastaFile, TP, EP, "Chr1", states, 50)
-			sys.stderr.write("Starting sample "+samNum+"\n")
-			#window(chromLens, experiment, fastaFile, TP, EP, "Chr1", states)
-
-def window(chromLens, experiment, fastaFile, TP, EP, chrom, states):
-	starts, methSeq = parseChrom(fastaFile, experiment, chrom, 1, chromLens)
-	statePath = viterbi(TP, EP, methSeq, states, fastaFile, chrom)
-	printPath(starts, statePath, states, chromLens, chrom)
-
-def useVoting(chromLens, experiment, fastaFile, TP, EP, chrom, states, slideSize):
-	voteArray = makeVoteArray(chromLens["Chr1"])
-	for start in xrange(1,windowSize,slideSize):
-		starts, methSeq = parseChrom(fastaFile, experiment, "Chr1", start, chromLens)
-		statePath = viterbi(TP, EP, methSeq, states, fastaFile, "Chr1")
-		del methSeq
-		vote(voteArray, statePath, starts)
-		print "%d/%d" % (start, windowSize)
-	#plotVotes(states, chromLens, chrom, voteArray)
-	#maxVotes = np.argmax(voteArray, axis=1)
-	TEvotes = voteArray[:,3] >= 5
-	maxVotes = np.zeros(len(TEvotes))
-	maxVotes[TEvotes] = 3
-	#print TEs
-	printFeatures(maxVotes, 3, states)
-
-def printFeatures(maxVotes, targetIndex, states):
-	features = []
-	start = 0
-	end = 0
-	for index in xrange(len(maxVotes)):
-		if maxVotes[index] == 3:
-			if start == 0:
-				start = index+1
-			else:
-				end = index+1
-		else:
-			if end != 0:
-				features.append((start,end))
-				start,end = 0,0
-	for i in features:
-		print "Chr1\t%s\t%d\t%d" % (states[targetIndex], i[0], i[1])
-
-def plotVotes(states, chromLens, chrom, voteArray):
-	plt.figure(0)
-	for state in xrange(len(states)):
-		X = np.arange(1,chromLens[chrom]+1,dtype="float")
-		sums = np.sum(voteArray,axis=1,dtype="float")
-		Y = voteArray[:,state]/sums
-		Y[0] = 0
-		Y[-1] = 0
-		plt.fill(X,Y,alpha=0.3,label=states[state])
-	plt.legend()
-	plt.show()
-
-def vote(voteArray, statePath, starts):
-	for index in xrange(len(statePath)):
-		state = statePath[index]
-		startIndex = starts[index]-1
-		endIndex = startIndex+windowSize
-		voteArray[startIndex:endIndex,state] += 1
-
-def makeVoteArray(size):
-	return np.zeros((size,5), dtype=int)
-
-def printPath(starts, statePath, states, chromLens, chrom):
-	for i in xrange(len(statePath)):
-		if statePath[i] == 3:
-			start = starts[i]
-			end = start+windowSize-1
-			print "%s\t%d\t%d" % (chrom, start, end)
-
-def viterbi(TP, EP, methSeq, states, fastaFile, chrom):
-	#TP[from][to]
-	startProb = [0.0, -np.inf, -np.inf, -np.inf, -np.inf]
-	TP = np.log2(TP)
-	numStates = len(states)
-	#print states
-	pathMat = np.empty([len(states), len(methSeq)], dtype=int)
-	probMat = np.empty([len(states), len(methSeq)])
-	pathMat[:,0] = -1
-	for i in xrange(numStates):
-		probMat[i,0] = startProb[i]+calcEmission(EP[i], methSeq[0])
-	for i in xrange(1,len(methSeq)):
-		if sum(np.isnan(methSeq[i])) == 3:
-				pMax = np.amax(probMat[:,i-1])
-				pMaxIndex = probMat[:,i-1].argmax()
-				for x in xrange(numStates):
-					pathMat[x,i] = pMaxIndex
-					probMat[x,i] = -np.inf
-				probMat[pMaxIndex,i] = pMax
-		else:
-			for x in xrange(numStates):
-				priors = []
-				for y in xrange(numStates):
-					priors.append(TP[y][x]+probMat[y,i-1])
-				pMax = max(priors)
-				pMaxIndex = priors.index(pMax)
-				pathMat[x,i]= pMaxIndex
-				probMat[x,i] = pMax+calcEmission(EP[x], methSeq[i])
-	maxProb = np.amax(probMat[:,-1])
-	maxIndex = probMat[:,-1].argmax()
-	path = [maxIndex]
-	for i in xrange(len(methSeq)-1,0,-1): # backtrace path
-		path.append(pathMat[path[-1],i])
-	#print "plog: %.3f" % (maxProb)
-	path = path[::-1]
-	return path
-
-def calcEmission(stateEP, data): #do something with this
-	#CG, CHH, CHG
-	total = 0.0
-	for i in xrange(len(data)):
-		if data[i] == 1:
-			total += stateEP[i][9]
-		elif np.isnan(data[i]):
-			pass
-		else:
-			total += stateEP[i][int(data[i])]
-	return total
-	
-def parseChrom(fastaFile, experiment, chrom, start, chromLens):
-	FA = pysam.Fastafile(fastaFile)
-	chromLen = chromLens[chrom]
-	starts = np.arange(start, chromLen+1, windowSize, dtype=int)
-	if starts[-1]+windowSize-1 > chromLen:
-		starts = starts[:-1]
-	methSeq = range(len(starts))
-	for index in xrange(len(starts)):
-		start = starts[index]	#1-indexed
-		end = start+windowSize-1
-		methFreqs = getFreqs(FA, experiment, chrom, start, end)
-		methSeq[index] = methFreqs[:]
-	return (starts, methSeq)
-
-def getFreqs(FA, experiment, chrom, start, end):
-	output = []
-	Y = np.zeros(3,end-start+1)	
-	C = np.zeros(3,end-start+1, dtype=int)
-	for methReplicate in experiment:
-		methA, contA = methReplicate.fetch(chrom=chrom, start=start, end=end)
-		methNA = np.array(methA)
-		contNA = np.array(contA)
-		for methIndex in xrange(3):
-			methLocs = contNA == methIndex+1 || contNA == methIndex+3
-			Y[methIndex,methLocs] += methNA[methLocs]
-			C[methIndex,methLocs] += contNA[methLocs]
-	for methIndex in xrange(3):
-		if sum(C[methIndex]) == 0:
-			output.append(np.nan)
-		else:
-			output.append(np.mean(Y[methIndex,C[methIndex,C[methIndex] > 0]]))
-	return tuple(output)
-	
-def parseEP(E):
-	#E[state][meth][count][index]
-	return map(parseState, E)
-def parseState(EPstate):
-	return map(parseMeth, EPstate)
-def parseMeth(EPmeth):
-	# added a pseudo-count so regions to reduce stringency
-	total = float(EPmeth[1]+len(EPmeth[0]))
-	return map(lambda x: np.log2((x+1.0)/total), EPmeth[0])
---
+++ team/annotate.py
@@ -0,0 +1,197 @@
+import numpy as np
+from team import getChromSizes
+from team import methTypes
+import sys
+
+def regionFinder(E,TP,fa,gff): #get this integrated
+	states = gff.values()
+	chromSizes = getChromSizes(fa+'.fai')
+	#TP[from][to]
+	# Emissions ##############
+	#[ 0.   0.1  0.2  0.3  0.4  0.5  0.6  0.7  0.8  0.9  1. ]
+	#[0 1)
+	#("NC","G","TG","TE","PG")
+
+	#get emission probabilities parsed like this
+	#pass in state name order
+	#E[state][meth][count][index]
+
+	print E
+	print TP
+	print chromSizes
+	print states
+
+	EP = parseEP(E,gff) #EP[sample][state][meth][index]
+	print EP
+	sys.exit()
+
+	for sampleName in self.samples:
+		methIndexes = []
+		for mf in self.samples[sampleName]:
+			methIndexes.append(MethIndex(mf,fa))
+			useVoting(chromLens, experiment, fastaFile, TP, EP, "Chr1", states, 50)
+			sys.stderr.write("Starting sample "+samNum+"\n")
+			#window(chromLens, experiment, fastaFile, TP, EP, "Chr1", states)
+
+def window(chromLens, experiment, fastaFile, TP, EP, chrom, states):
+	starts, methSeq = parseChrom(fastaFile, experiment, chrom, 1, chromLens)
+	statePath = viterbi(TP, EP, methSeq, states, fastaFile, chrom)
+	printPath(starts, statePath, states, chromLens, chrom)
+
+def useVoting(chromLens, experiment, fastaFile, TP, EP, chrom, states, slideSize):
+	voteArray = makeVoteArray(chromLens["Chr1"])
+	for start in xrange(1,windowSize,slideSize):
+		starts, methSeq = parseChrom(fastaFile, experiment, "Chr1", start, chromLens)
+		statePath = viterbi(TP, EP, methSeq, states, fastaFile, "Chr1")
+		del methSeq
+		vote(voteArray, statePath, starts)
+		print "%d/%d" % (start, windowSize)
+	#plotVotes(states, chromLens, chrom, voteArray)
+	#maxVotes = np.argmax(voteArray, axis=1)
+	TEvotes = voteArray[:,3] >= 5
+	maxVotes = np.zeros(len(TEvotes))
+	maxVotes[TEvotes] = 3
+	#print TEs
+	printFeatures(maxVotes, 3, states)
+
+def printFeatures(maxVotes, targetIndex, states):
+	features = []
+	start = 0
+	end = 0
+	for index in xrange(len(maxVotes)):
+		if maxVotes[index] == 3:
+			if start == 0:
+				start = index+1
+			else:
+				end = index+1
+		else:
+			if end != 0:
+				features.append((start,end))
+				start,end = 0,0
+	for i in features:
+		print "Chr1\t%s\t%d\t%d" % (states[targetIndex], i[0], i[1])
+
+def plotVotes(states, chromLens, chrom, voteArray):
+	plt.figure(0)
+	for state in xrange(len(states)):
+		X = np.arange(1,chromLens[chrom]+1,dtype="float")
+		sums = np.sum(voteArray,axis=1,dtype="float")
+		Y = voteArray[:,state]/sums
+		Y[0] = 0
+		Y[-1] = 0
+		plt.fill(X,Y,alpha=0.3,label=states[state])
+	plt.legend()
+	plt.show()
+
+def vote(voteArray, statePath, starts):
+	for index in xrange(len(statePath)):
+		state = statePath[index]
+		startIndex = starts[index]-1
+		endIndex = startIndex+windowSize
+		voteArray[startIndex:endIndex,state] += 1
+
+def makeVoteArray(size):
+	return np.zeros((size,5), dtype=int)
+
+def printPath(starts, statePath, states, chromLens, chrom):
+	for i in xrange(len(statePath)):
+		if statePath[i] == 3:
+			start = starts[i]
+			end = start+windowSize-1
+			print "%s\t%d\t%d" % (chrom, start, end)
+
+def viterbi(TP, EP, methSeq, states, fastaFile, chrom):
+	#TP[from][to]
+	startProb = [0.0, -np.inf, -np.inf, -np.inf, -np.inf]
+	TP = np.log2(TP)
+	numStates = len(states)
+	#print states
+	pathMat = np.empty([len(states), len(methSeq)], dtype=int)
+	probMat = np.empty([len(states), len(methSeq)])
+	pathMat[:,0] = -1
+	for i in xrange(numStates):
+		probMat[i,0] = startProb[i]+calcEmission(EP[i], methSeq[0])
+	for i in xrange(1,len(methSeq)):
+		if sum(np.isnan(methSeq[i])) == 3:
+				pMax = np.amax(probMat[:,i-1])
+				pMaxIndex = probMat[:,i-1].argmax()
+				for x in xrange(numStates):
+					pathMat[x,i] = pMaxIndex
+					probMat[x,i] = -np.inf
+				probMat[pMaxIndex,i] = pMax
+		else:
+			for x in xrange(numStates):
+				priors = []
+				for y in xrange(numStates):
+					priors.append(TP[y][x]+probMat[y,i-1])
+				pMax = max(priors)
+				pMaxIndex = priors.index(pMax)
+				pathMat[x,i]= pMaxIndex
+				probMat[x,i] = pMax+calcEmission(EP[x], methSeq[i])
+	maxProb = np.amax(probMat[:,-1])
+	maxIndex = probMat[:,-1].argmax()
+	path = [maxIndex]
+	for i in xrange(len(methSeq)-1,0,-1): # backtrace path
+		path.append(pathMat[path[-1],i])
+	#print "plog: %.3f" % (maxProb)
+	path = path[::-1]
+	return path
+
+def calcEmission(stateEP, data): #do something with this
+	#CG, CHH, CHG
+	total = 0.0
+	for i in xrange(len(data)):
+		if data[i] == 1:
+			total += stateEP[i][9]
+		elif np.isnan(data[i]):
+			pass
+		else:
+			total += stateEP[i][int(data[i])]
+	return total
+	
+def parseChrom(fastaFile, experiment, chrom, start, chromLens):
+	FA = pysam.Fastafile(fastaFile)
+	chromLen = chromLens[chrom]
+	starts = np.arange(start, chromLen+1, windowSize, dtype=int)
+	if starts[-1]+windowSize-1 > chromLen:
+		starts = starts[:-1]
+	methSeq = range(len(starts))
+	for index in xrange(len(starts)):
+		start = starts[index]	#1-indexed
+		end = start+windowSize-1
+		methFreqs = getFreqs(FA, experiment, chrom, start, end)
+		methSeq[index] = methFreqs[:]
+	return (starts, methSeq)
+
+def getFreqs(FA, experiment, chrom, start, end):
+	output = []
+	Y = np.zeros(3,end-start+1)	
+	C = np.zeros(3,end-start+1, dtype=int)
+	for methReplicate in experiment:
+		methA, contA = methReplicate.fetch(chrom=chrom, start=start, end=end)
+		methNA = np.array(methA)
+		contNA = np.array(contA)
+		for methIndex in xrange(3):
+			methLocs = np.logical_or(contNA == methIndex+1, contNA == methIndex+3)
+			Y[methIndex,methLocs] += methNA[methLocs]
+			C[methIndex,methLocs] += contNA[methLocs]
+	for methIndex in xrange(3):
+		if sum(C[methIndex]) == 0:
+			output.append(np.nan)
+		else:
+			output.append(np.mean(Y[methIndex,C[methIndex,C[methIndex] > 0]]))
+	return tuple(output)
+	
+def parseEP(E, gff, pseudoCount=1):
+	#self.emissions[sampleName][gff][methType] array of 10 hist values.
+	#EP[sample][state][meth][index]
+	EP = {}
+	for sample in E:
+		EP[sample] = []
+		for gffFile in gff:
+			methData = []
+			for methType in methTypes:
+				total = sum(E[sample][gffFile][methType])
+				methData.append(map(lambda x: np.log2((x+float(pseudoCount))/float(total)), E[sample][gffFile][methType]))
+			EP[sample].append(methData)
+	return EP
